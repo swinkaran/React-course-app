6c6429bffafc590c200cb9bd3d643c40597b02b8
Swinkaran <swinkaran@hotmail.com>
2022-05-26 15:17:44 +1000

    List courses
    Added course list page
    Added basic routing
    Added header

---
+++ ps-flux/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage(){
+   return (
+       <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, Flux, and React Router learing</p>    
+    </div>
+   );
+}
+
+export default HomePage;
